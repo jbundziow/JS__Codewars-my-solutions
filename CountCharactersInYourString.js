fa95cd91876a080198267dd2fefd8f52a0921a71
jbundziow <jakub.bundziow@gmail.com>
2022-10-20 09:27:47 +0200

    final version

--- CountCharactersInYourString.js
+++ CountCharactersInYourString.js
@@ -1,33 +1,32 @@
 // The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 // What if the string is empty? Then the result should be empty object literal, {}.
 
-
 //function count (string) {  
 //return 0;
 //}
 
-const input = 'aba';
+function count (string) {  
 
-let chars = input.split('');
-
-let uniqueChars = [];
-chars.forEach((element) => {
-    if (!uniqueChars.includes(element)) {
-        uniqueChars.push(element);
+    let chars = string.split('');
+    
+    let uniqueChars = [];
+    chars.forEach((element) => {
+        if (!uniqueChars.includes(element)) {
+            uniqueChars.push(element);
+        }
+    });
+    
+    let result = {};
+    uniqueChars.forEach((element) => {
+    let i = 0;
+    chars.forEach((item) => {
+    if (item === element ) {i++;}
+    });
+    result[`${element}`] = i;
+    });
+      
+       return result;
     }
-});
 
-console.log(chars);
-console.log(uniqueChars);
-
-let result = {};
-uniqueChars.forEach((element) => {
-let i = 0;
-chars.forEach((item) => {
-if (item === element ) {i++;}
-})
-//result.push(`'${element}': ${i}`);
-result[`${element}`] = i;
-});
-
-console.log(result);
+    const str = 'abba';
+    console.log(count(str));
